9bae8611eb569601fd2a29f444ebe7aa54d5e1e8
sigurdandris <sigurd.andris.sorengen@gmail.com>
2024-09-30 10:47:31 +0200

    hello

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("moren din er veldig fin ;)!");
-        System.out.println("ja");
+        System.out.println("mordi også");
     }
 }
