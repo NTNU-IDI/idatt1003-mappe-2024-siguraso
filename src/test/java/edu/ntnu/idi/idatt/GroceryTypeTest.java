62e8ccaae060f81b089da40e9dc2e3e0f08dc314
sigurdandris <sigurd.andris.sorengen@gmail.com>
2024-10-28 12:50:38 +0100

    renamed GroceryTest to GroceryTypeTest

--- src/test/java/edu/ntnu/idi/idatt/GroceryTypeTest.java
+++ src/test/java/edu/ntnu/idi/idatt/GroceryTypeTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class GroceryTest {
+class GroceryTypeTest {
 
   GroceryType surST = new GroceryType("Surstrømming", "stk.");
 
