44bf608c7f10e5f45dc3ebf6f8c9d1245bafe9fe
sigurdandris <sigurd.andris.sorengen@gmail.com>
2024-11-28 19:10:12 +0100

    completed GroceryType test class

--- src/test/java/edu/ntnu/idi/idatt/models/TestGroceryType.java
+++ src/test/java/edu/ntnu/idi/idatt/models/TestGroceryType.java
@@ -13,7 +13,7 @@ class TestGroceryType {
 
   @BeforeEach
   void setUp() {
-    groceryType = new GroceryType("tomato", "kg");
+    groceryType = new GroceryType("Tomato", "kg");
   }
 
 
@@ -61,8 +61,45 @@ class TestGroceryType {
     void newGroceryTypeDoesNotThrowException() {
       try {
         GroceryType newGroceryType = new GroceryType("null", "null");
+
       } catch (Exception e) {
         fail("newGroceryTypeDoesNotThrowException() failed, since it threw an exception.");
+      }
+    }
+
+    @Test
+    @DisplayName("getter methods should not throw exceptions.")
+    void groceryInstanceGetterMethodsDoesNotThrowException() {
+      try {
+        assertEquals("Tomato", groceryType.getName());
+        assertEquals("tomato", groceryType.getNameLowerCase());
+        assertEquals("kg", groceryType.getMeasurementUnit());
+      } catch (Exception e) {
+        fail("verifyGetterMethodsDoesNotThrowException() failed, since it threw an exception.");
+      }
+    }
+
+    @Test
+    @DisplayName("setName should not throw an exception when the name is less than 30 chars.")
+    void groceryInstanceSetNameDoesNotThrowException() {
+      try {
+        groceryType.setName("mmmm tomato");
+        assertEquals("mmmm tomato", groceryType.getName());
+      } catch (Exception e) {
+        fail("groceryInstanceSetNameDoesNotThrowException() failed, since it threw an exception.");
+      }
+    }
+
+    @Test
+    @DisplayName(
+        "setMeasurementUnit should not throw an exception when the unit is less than 16 chars.")
+    void groceryInstanceSetMeasurementUnitDoesNotThrowException() {
+      try {
+        groceryType.setMeasurementUnit("g");
+        assertEquals("g", groceryType.getMeasurementUnit());
+      } catch (Exception e) {
+        fail("groceryInstanceSetMeasurementUnitDoesNotThrowException() failed, since it threw an "
+            + "exception.");
       }
     }
   }
